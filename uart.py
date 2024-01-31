9e21f005c269a7897f0acad0d6eff70ced2f7a27
정진우 <91660287+jjw6712@users.noreply.github.com>
2024-02-01 00:27:54 +0900

    패킷 구조 수정

--- uart.py
+++ uart.py
@@ -1,12 +1,12 @@
+import serial
 import struct
 import random
-import serial
-import time
 
 uart_port = 'COM6'  # 컴퓨터 UART 포트로 변경하세요
 baud_rate = 115200   # 바우드 레이트
 
 # 데이터 패킷 생성 함수
+
 
 def create_data_packet():
     stx = 0x02
@@ -15,20 +15,26 @@ def create_data_packet():
     water_level = random.randint(0x0000, 0xFFFF)
     humidity = random.randint(0x00, 0x64)
     battery = 0x07
-    etx = 0x03
     drive = 0x01
     stop = 0x00
     wh = 0x00
     blackout = 0x00
+    etx = 0x03
 
-    # 체크섬 계산 (STX부터 ETX까지 모든 바이트의 XOR 연산)
-    checksum = stx ^ cmd ^ (pressure >> 8) ^ (pressure & 0xFF) ^ \
-               (water_level >> 8) ^ (water_level & 0xFF) ^ \
-               humidity ^ battery ^ etx ^ drive ^ stop ^ wh ^ blackout
+    # 체크섬 계산 최적화: 직접적인 XOR 연산
+    checksum = (stx ^ cmd ^
+                (pressure >> 8) ^ (pressure & 0xFF) ^
+                (water_level >> 8) ^ (water_level & 0xFF) ^
+                humidity ^ battery ^ drive ^ stop ^ wh ^ blackout ^ etx)
 
     # 데이터 패킷 구성 및 바이트 패킹
-    data_packet = struct.pack('>B B H H B B B B B B B B', stx, cmd, pressure, water_level, humidity, battery, drive, stop, wh, blackout, etx, checksum)
+    data_packet = struct.pack('>B B H H B B B B B B B B', stx, cmd, pressure, water_level, humidity, battery, drive,
+                              stop, wh, blackout, etx, checksum)
     return data_packet
+
+# 패킷 생성 및 출력
+data_packet = create_data_packet()
+print(data_packet)
 
 
 
